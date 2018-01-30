9cb7b1ea9a0686f3273379369413ca3ff3927bc0
jared frankel <jaredfrankel10@gmail.com>
2018-01-30 17:38:22 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,2 +1,7 @@
-class CreateArtists < ActiveRecord::Migration
+class CreateArtists < ActiveRecord::Migration[5.0]
+  def change
+     create_table :artist do |t|
+       t.string :name
+     end
+   end
 end
